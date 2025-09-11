2d0ad68f5e429687849c0caea54f78e1d40d55f5
sulgard <sulgardworkshop@gmail.com>
2025-09-11 16:41:53 +0200

    minor backend change

--- backend/med-app/src/main/java/com/example/med_app/service/UserService.java
+++ backend/med-app/src/main/java/com/example/med_app/service/UserService.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
